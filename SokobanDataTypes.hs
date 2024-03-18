588df54eba4a6b6402cca9d7200310770cccb1b5
KLCaron <kellencaron@gmail.com>
2024-03-17 22:50:19 -0600

    data types are done maybe

--- SokobanDataTypes.hs
+++ SokobanDataTypes.hs
@@ -2,8 +2,9 @@
 module SokobanDataTypes (
   TileType(..),
   Direction(..),
-  SokobanPuzzle
+  SokobanPuzzle(..)
 ) where
 
 data TileType = Wall | Empty | Player | PlayerGoal | Box | BoxGoal | Goal deriving (Eq, Show)
 data Direction = Up | Down | Left | Right deriving (Eq, Show)
+data SokobanPuzzle = SokobanPuzzle { gameState :: [[TileType]]} deriving (Eq, Show)
